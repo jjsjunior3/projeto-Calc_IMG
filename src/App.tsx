075486023b0c0b6a942afda062b7c2605a794d37
Junior Santos <junior.ead@hotmail.com>
2023-01-18 21:21:46 -0300

    Foi desenvolvido a função do calculo e boa parte front

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
 import styles from './App.module.css';
-import poweredImage from './assets/powered.png'
+import poweredImage from './assets/powered.png';
+import { GridItem } from './components/GridItem';
+
+import { levels, calculateImc} from './helpers/imc';
 
 const App = () => {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  const handleCalculateButton =() => {
+    if (heightField && weightField) {
+
+    } else {
+      alert("Digite todos os campos");
+    }
+  }
+
   return (
     <div className={styles.main}>
       <header>
@@ -11,10 +25,30 @@ const App = () => {
       </header>
       <div className={styles.container}>
         <div className={styles.leftSide}>
-          ...
+          <h1>Calcule o seu IMC.</h1>
+          <p>IMC é a sigla para Índice de Mass Corpórea, parâmetro adotado pela Organização Mundial da Saúde para calcular o peso ideal de cada pessoa.</p>
+          <input
+            type="number"
+            placeholder="Digite a sua altura. Ex: 1.5 (em metros)"
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+          />
+          <input
+            type="number"
+            placeholder="Digite o seu peso. Ex: 75.3 (em Kg)"
+            value={weightField > 0 ? weightField : ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}
+          />
+
+          <button onClick={handleCalculateButton}>Calcular</button>
+
         </div>
         <div className={styles.rightSide}>
-          ...
+          <div className={styles.grid}>
+            {levels.map((item, key) => (
+              <GridItem key={key} item={item}/>
+            ))}
+          </div>
         </div>
 
       </div>
