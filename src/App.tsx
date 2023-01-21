e0b0db54733db02c6c8f15f376cabee9ff6cfae9
Junior Santos <junior.ead@hotmail.com>
2023-01-20 22:46:33 -0300

    finalizando o projeto com responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImage from './assets/powered.png';
+import leftArrowImage from './assets/leftarrow.png';
 import { GridItem } from './components/GridItem';
 
 import { levels, calculateImc, Level} from './helpers/imc';
@@ -16,6 +17,12 @@ const App = () => {
     } else {
       alert("Digite todos os campos");
     }
+  }
+
+  const handleBackButton = () => {
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
   }
 
   return (
@@ -34,15 +41,18 @@ const App = () => {
             placeholder="Digite a sua altura. Ex: 1.5 (em metros)"
             value={heightField > 0 ? heightField : ''}
             onChange={e => setHeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false }
           />
           <input
             type="number"
             placeholder="Digite o seu peso. Ex: 75.3 (em Kg)"
             value={weightField > 0 ? weightField : ''}
             onChange={e => setWeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false }
+
           />
 
-          <button onClick={handleCalculateButton}>Calcular</button>
+          <button onClick={handleCalculateButton} disabled={toShow ? true : false }>Calcular</button>
 
         </div>
         <div className={styles.rightSide}>
@@ -55,7 +65,9 @@ const App = () => {
           }
           {toShow &&
             <div className={styles.rightBig}>
-              <div className={styles.rightArrow}></div>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImage} alt="" width={25}/>
+              </div>
               <GridItem item={toShow} />
             </div>
           }
--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -18,8 +18,10 @@ export const calculateImc = (height: number, weight: number) => {
 
     for(let i in levels) {
         if (imc >= levels[i].imc[0] && imc < levels[i].imc[1]) {
-            levels[i].yourImc = parseFloat(imc.toFixed(2));
-            return levels[i];
+            let levelCopy: Level = { ...levels[i] };
+
+            levelCopy.yourImc = parseFloat(imc.toFixed(2));
+            return levelCopy;
         }
     }
 
